e3256a7937ef3fece1a4ffb8762b43ed1be45963
huntrag <r29aghavgade@gmail.com>
2022-03-08 12:59:36 +0530

    Initial Setup

--- sec/base/views.py
+++ sec/base/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from utils import get_collection
+from . import utils
 from django.shortcuts import render, redirect
-from django.http import HttpRespons
+from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
     return render(request, 'base/home.html')
 
 def comp(request,pk):
-    db=get_collection('comp-names')
+    db=utils.get_collection('comp-names')
     comp=db.find_one({'_id':pk})
     context={'comp':comp}
     return render(request, 'base/room.html', context)
