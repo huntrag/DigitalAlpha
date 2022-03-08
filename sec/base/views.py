d8f3b8e0ce46662c9382cfb82ab99d6bb21c4494
huntrag <r29aghavgade@gmail.com>
2022-03-08 14:46:54 +0530

    Ab Logo aa raha hai

--- sec/base/views.py
+++ sec/base/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from . import utils
+import json
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
+import pymongo
+from dotenv import load_dotenv
+import datetime
+import os
+
+load_dotenv()
+MONGODB_URI = os.environ['MONGODB_URI']
+
+client = pymongo.MongoClient(MONGODB_URI)
+db1=client['tech-meet']
+db=db1['comp_names']
+
 
 def home(request):
-    return render(request, 'base/home.html')
+    q=request.GET.get('q') if request.GET.get('q')!=None else ''
+    
+    comp=db.find_one({'ticker':q})
+    print(comp)
+    context={'comp':comp}
+    return render(request, 'base/home.html',context)
 
 def comp(request,pk):
-    db=utils.get_collection('comp-names')
     comp=db.find_one({'_id':pk})
-    context={'comp':comp}
-    return render(request, 'base/room.html', context)
+    context={'comp':comp} 
+    return render(request, 'base/comp.html',context)
---
+++ sec/sec/views.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import datetime
+import os
+
+load_dotenv()
+MONGODB_URI = os.environ['MONGODB_URI']
+
+client = MongoClient(MONGODB_URI)
