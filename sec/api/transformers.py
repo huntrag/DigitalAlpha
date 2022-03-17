5e1fef1f0e8e6a13add1f6ea5ac77c369df99780
Kaushik Dey <Kaushikdey647@gmail.com>
2022-03-17 19:25:23 +0530

    Update transformers.py

--- sec/api/transformers.py
+++ sec/api/transformers.py
@@ -1,5 +1,5 @@
 from transformers import BertTokenizer, BertForSequenceClassification
 finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone',num_labels=3)
 tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
-finbert.save_pretrained("C:\\Users\\Kaushik Dey\\Documents\\DigitalAlpha\\sec\\finbert")
+finbert.save_pretrained("C:\\Users\\Kaushik Dey\\Documents\\DigitalAlpha\\sec\\finbert-tone")
 tokenizer.save_pretrained("C:\\Users\\Kaushik Dey\\Documents\\DigitalAlpha\\sec\\finbert-tone")
